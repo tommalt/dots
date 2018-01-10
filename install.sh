2fe625e46f64b601459b0f01537bc46f81339b89
thomasmaltese <thomasmaltese64@gmail.com>
2018-01-10 02:38:24 -0500

    update install script w/ printing info, handling .h files

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+function exclude {
+	if [[ "$1" == "README" ]] || [[ "$1" == "install.sh" ]] || [[ $1 == *.h ]]; then
+		return 0
+	fi
+	return 1
+}
 files=`git ls-files`
 for f in $files; do
-	if [[ "$f" == "README" ]] || [[ "$f" == "install.sh" ]]; then
+	if exclude "$f"; then
 		continue;
 	fi
 	target="$HOME/dots/$f";
 	cp "$target" "$HOME/$f";
+	echo "Installed $HOME/$f";
 done
 
