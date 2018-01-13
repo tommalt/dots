516563e67b2b82a4512cbeacb7a18fb1b2cab8d8
thomasmaltese <thomasmaltese64@gmail.com>
2018-01-13 02:19:12 -0500

    fixed install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-
+cd "$HOME/dots"
 function exclude {
 	if [[ "$1" == "README" ]] || [[ "$1" == "install.sh" ]] || [[ $1 == *.h ]]; then
 		return 0
@@ -13,8 +13,12 @@ for f in $files; do
 	if exclude "$f"; then
 		continue;
 	fi
-	target="$HOME/dots/$f";
-	cp "$target" "$HOME/$f";
-	echo "Installed $HOME/$f";
+	dest="$HOME/$f"
+	dir=$(dirname "$dest")
+	if [ ! -d "$dir" ]; then
+		mkdir -p "$dir";
+	fi
+	src="$HOME/dots/$f";
+	echo "cp $src $dest"
+	cp "$src" "$dest";
 done
-
