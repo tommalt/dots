6bf8034a39a8537e5218354a3cc63a22ff72d258
thomasmaltese <thomasmaltese64@gmail.com>
2018-01-24 17:17:02 -0500

    fish vi key bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,3 +15,12 @@ alias time="time -p"
 set -gx EDITOR vis
 set -gx LD_LIBRARY_PATH /usr/local/lib
 set -gx PKG_CONFIG_PATH /usr/local/lib/pkgconfig
+
+function hybrid_keybindings --description "Vi-style bindings stat inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+
+set -g fish_key_bindings hybrid_keybindings
