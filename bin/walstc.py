b60509f0f46689b40c315134de37ea84f2f9ebd3
thomasmaltese <thomasmaltese64@gmail.com>
2018-01-26 06:51:48 -0500

    fixed errors in walstc(.py)

--- bin/walstc.py
+++ bin/walstc.py
@@ -25,6 +25,20 @@ def isvardecl(pattern, line, names, types):
     semi = line.find(';') != -1
     return (all((t in tfound for t in types)) and
             any((n in nfound for n in names)) and semi)
+
+def extract_var_names(patchfile):
+    names=[]
+    types = set()
+    with open(patchfile,'r') as f:
+        for line in f:
+            line=line.strip()
+            if len(line) > 2 and line[-1] == ';':
+                toks= list(filter(str.isalpha, line.split(' ')))
+                names.append(toks[-1])
+                for i in range(0, len(toks)-1):
+                    types.add(toks[i])
+    return (types,names)
+
 def main():
     argc = len(sys.argv);
     if argc < 3:
@@ -32,12 +46,10 @@ def main():
         sys.exit(1)
     fname = sys.argv[1]
     include = '#include "{}"'.format(os.path.abspath(sys.argv[2]))
+    types, names = extract_var_names(sys.argv[2])
     state = 0 #0=out, 1 = in
     found = False
     pattern = re.compile("\w+");
-    cnames = ['defaultbg','defaultfg','defaultcs','defaultrcs',
-            'mousefg','mousebg'] #exclude these
-    ctypes = ['unsigned','int']
     with open(fname, "r") as f:
         for line in f:
             line_s=line.strip()
@@ -50,10 +62,8 @@ def main():
                 if line_s.find('};') != -1:
                     state = 0
             else:
-                for name in cnames:
-                    if not isvardecl(pattern,line_s,cnames,ctypes):
-                        print(line, end='')
-                        break;
+                if not isvardecl(pattern,line_s,names,types):
+                    print(line, end='')
                     #else:
                         #print("{} is a vardecl".format(line))
 main()
