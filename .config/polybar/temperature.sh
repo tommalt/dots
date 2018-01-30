981de901790189fca9ad208039cefee6294011b5
Tom Maltese <thomasmaltese64@gmail.com>
2018-01-30 03:29:28 -0500

    spacing between temperature in polybar

--- .config/polybar/temperature.sh
+++ .config/polybar/temperature.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sensors|grep 'temp[0-9]' |awk '{ print substr($2,2,length($2)-5)}'|tr "\\n" " "| sed 's/ /°C/g'
+sensors|grep 'temp[0-9]' |awk '{ print substr($2,2,length($2)-5)}'|tr "\\n" " "| sed 's/ /°C /g'
