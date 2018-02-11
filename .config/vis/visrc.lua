0e754cbe74957cf234b5e7217561da98e6e034d3
thomasmaltese <thomasmaltese64@gmail.com>
2018-02-11 18:31:36 -0500

    changed vis colorscheme to acme

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -3,7 +3,7 @@ require('vis')
 
 vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
-	vis:command('set theme dark-16')
+	vis:command('set theme acme')
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
