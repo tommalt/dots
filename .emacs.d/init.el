c41543548b80dc0218b225197b241248a5b59c13
thomasmaltese <thomasmaltese64@gmail.com>
2018-04-23 00:05:22 -0400

    removed .emacs ; put 'smart-tabs' in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,10 +58,16 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit counsel-projectile company clean-aindent-mode anzu smart-tabs-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; C and C++ indent
+(setq-default c-default-style "linux")
+(smart-tabs-add-language-support c++ c++-mode-hook
+                                 ((c-indent-line . c-basic-offset)
+                                  (c-indent-region . c-basic-offset)))
