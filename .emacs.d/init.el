49f98c7335557de3a474e7c134d64686ecbe8a19
thomasmaltese <thomasmaltese64@gmail.com>
2018-05-25 15:14:09 -0400

    emacs evil, slime. shortcut to edit config file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,6 +24,9 @@
 (require 'setup-cedet)
 (require 'setup-editing)
 
+;; use F6 to quickly edit this file
+(global-set-key (kbd "<f6>") (lambda () (interactive)(find-file "~/.emacs.d/init.el")))
+
 ;; prevent emacs from freezing when pressing Ctrl+z
 (global-unset-key (kbd "C-z"))
 
@@ -58,7 +61,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit counsel-projectile company clean-aindent-mode anzu smart-tabs-mode))))
+    (evil buffer-move slime zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit counsel-projectile company clean-aindent-mode anzu smart-tabs-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -71,3 +74,44 @@
 (smart-tabs-add-language-support c++ c++-mode-hook
                                  ((c-indent-line . c-basic-offset)
                                   (c-indent-region . c-basic-offset)))
+
+;; SLIME
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
+
+;; font size. :height in units 1/10pt, (ex: :height 100 = 10pt font)
+(set-face-attribute 'default nil :height 90)
+(set-face-attribute 'mode-line nil :height 80)
+
+;; easier movement of buffers
+(global-set-key (kbd "<C-S-up>")     'buf-move-up)
+(global-set-key (kbd "<C-S-down>")   'buf-move-down)
+(global-set-key (kbd "<C-S-left>")   'buf-move-left)
+(global-set-key (kbd "<C-S-right>")  'buf-move-right)
+
+;; EVIL-MODE
+
+;; 'exact amount of vim' from http://puntoblogspot.blogspot.com.es/2014/01/evil-exact-amount-of-vim-in-emacs-but.html
+;;  and https://stackoverflow.com/questions/26572717/evil-emacs-is-there-a-way-to-disable-vim-like-keys-in-insert-mode
+(require 'evil)
+;; remove all keybindings in evil-insert-state, and redefind some
+(setcdr evil-insert-state-map nil)
+(define-key evil-insert-state-map (kbd "<ESC>") 'evil-normal-state)
+(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+(define-key evil-normal-state-map (kbd "[ m") 'beginning-of-defun)
+(define-key evil-normal-state-map (kbd "] m") 'end-of-defun)
+(define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+(define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+
+;; disable evil mode on a per-buffer basis
+(global-set-key (kbd "<f7>") 'evil-local-mode)
+(define-key evil-normal-state-map (kbd "<f7>") 'evil-local-mode)
+(define-key evil-insert-state-map (kbd "<f7>") 'evil-local-mode)
+
+(setq evil-jumps-cross-buffers nil) ;; for C-o and C-i to not cross buffers
+(provide 'emvil)
+
+;; C-r tries to 'redo', change back to reverse search
+(define-key evil-normal-state-map (kbd "<C-r>") 'isearch-backward)
+;; when sliming
+(evil-set-initial-state 'slime-repl 'emacs)
